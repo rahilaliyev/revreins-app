c0739ce0ab8d89894b4b58c3ec382465e63996d1
Rahil <rahil.eliyev@gmail.com>
2026-01-04 15:05:50 +0400

    adding logout logic

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -1,43 +1,92 @@
-import type { JSX } from 'react';
-import { useLocation } from 'react-router-dom';
+import { type JSX, type MouseEvent, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { colorPalette } from 'src/theme/colorpalette';
 
-import { Avatar, Badge, Box, Button, Divider, IconButton, Stack, Typography } from '@mui/material';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Button,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from '@mui/material';
 
-import { getPageTitle } from 'src/utils';
+import { ROUTES } from 'src/routes/paths';
+import { getPageTitle, removeAuthCookies } from 'src/utils';
 
 import { GlobalLineIcon, Notification3LineIcon, QuestionLineIcon, User6LineIcon } from 'src/assets/icons';
 
 const Header = (): JSX.Element => {
   const location = useLocation();
+  const navigate = useNavigate();
   const pageTitle = getPageTitle(location.pathname);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: MouseEvent<HTMLElement>): void => setAnchorEl(event.currentTarget);
+
+  const handleClose = (): void => {
+    setAnchorEl(null);
+    removeAuthCookies();
+    navigate(ROUTES.AUTH.SIGNIN.PATH);
+  };
 
   return (
-    <Stack py={2} px={4} justifyContent="space-between">
-      <Typography variant="body2" color="text.secondary">
-        {pageTitle}
-      </Typography>
-      <Stack>
-        <IconButton>
-          <Badge color="error" variant="dot">
-            <Notification3LineIcon />
-          </Badge>
-        </IconButton>
-        <Button variant="text" color="secondary" sx={{ gap: 4, ml: 1.5, mr: 2.5 }}>
-          <Typography variant="subtitle1">John Doe</Typography>
-          <Avatar sx={{ bgcolor: colorPalette.primary.bg }}>
-            <User6LineIcon pathFill={colorPalette.primary.main} />
-          </Avatar>
-        </Button>
-        <Divider orientation="vertical" flexItem />
-        <IconButton sx={{ ml: 6, mr: 3 }}>
-          <GlobalLineIcon />
-        </IconButton>
-        <IconButton>
-          <QuestionLineIcon />
-        </IconButton>
+    <AppBar position="static" color="transparent" elevation={0}>
+      <Stack py={2} px={4} justifyContent="space-between">
+        <Typography variant="body2" color="text.secondary">
+          {pageTitle}
+        </Typography>
+        <Stack>
+          <IconButton>
+            <Badge color="error" variant="dot">
+              <Notification3LineIcon />
+            </Badge>
+          </IconButton>
+          <Button
+            variant="text"
+            color="secondary"
+            sx={{ gap: 4, ml: 1.5, mr: 2.5 }}
+            onClick={handleMenu}
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+          >
+            <Typography variant="subtitle1">John Doe</Typography>
+            <Avatar sx={{ bgcolor: colorPalette.primary.bg }}>
+              <User6LineIcon pathFill={colorPalette.primary.main} />
+            </Avatar>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </Button>
+          <Divider orientation="vertical" flexItem />
+          <IconButton sx={{ ml: 6, mr: 3 }}>
+            <GlobalLineIcon />
+          </IconButton>
+          <IconButton>
+            <QuestionLineIcon />
+          </IconButton>
+        </Stack>
       </Stack>
-    </Stack>
+    </AppBar>
   );
 };
 
